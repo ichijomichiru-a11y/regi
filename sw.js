9a9cdf88170bfa5e7900b92d56d576be997bf229
つみ <ichijomichiru@gmail.com>
2026-04-09 23:00:27 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
-self.addEventListener('fetch', () => {}); // すべてのキャッシュ機能を停止する命令
+const CACHE_NAME = 'bakery-regi-v7'; // 以前より新しい番号にする
+
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+// インストール時にファイルを保存する
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ネットワークがない時は保存データから表示する
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
